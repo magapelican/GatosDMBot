7cd5a2ce417e18456b6a51078cbba8bdbe748a62
lain <hadjik777@gmail.com>
2023-11-20 00:45:51 +0300

    Bugfix

--- handlers.py
+++ handlers.py
@@ -15,7 +15,7 @@ async def cmd_start(message: Message):
     await message.answer("Salam, vac!")
 
 
-@router.message(F.photo, IsAdminFilter())
+@router.message(F.photo, ~IsAdminFilter())
 async def send_on_message(message: Message, bot: Bot):
     photo_id = message.photo[-1].file_id  # message.photo[-1] to get pic of biggest size
     user_name = message.from_user.first_name
@@ -41,6 +41,8 @@ async def post_img_to_channel(callback: CallbackQuery, bot: Bot):
 
 @router.callback_query(F.data.startswith("do_not_post_img"))
 async def dont_post_img_to_channel(callback: CallbackQuery, bot: Bot):
-    sender_id = callback.data.split("_")[-1]
+    msg_id = callback.data.split("_")[-1]
+    sender_id = callback.data.split("_")[-2]
     await bot.send_message(sender_id, "Админу не понравилась твоя публикация((")
     await callback.message.delete_reply_markup()
+    database.delete_data_from_db(msg_id)
--- keyboards.py
+++ keyboards.py
@@ -6,6 +6,6 @@ def get_post_or_not_kb(msg_id, sender_id) -> InlineKeyboardMarkup:
     builder = InlineKeyboardBuilder()
     builder.add(
         InlineKeyboardButton(text="✅", callback_data=f"post_img_{msg_id}"),
-        InlineKeyboardButton(text="❌", callback_data=f"do_not_post_img_{sender_id}")
+        InlineKeyboardButton(text="❌", callback_data=f"do_not_post_img_{sender_id}_{msg_id}")
     )
     return builder.as_markup()
