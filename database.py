5f400855ac68575bd6c35ec4bd8f0f0e07bbc41b
lain <hadjik777@gmail.com>
2023-11-24 19:08:51 +0300

    Add param types & return types

--- database.py
+++ database.py
@@ -1,12 +1,13 @@
 import sqlite3
 import sys
+from typing import Tuple
 
 con = sqlite3.connect("predlozhka.db")
 
 con.execute("""
     CREATE TABLE IF NOT EXISTS user_data (
-        message_id,
-        file_id
+        message_id INTEGER PRIMARY KEY,
+        file_id TEXT UNIQUE
     )
 """)
 
@@ -14,15 +15,15 @@ if "user_data" not in con.execute("SELECT name FROM sqlite_master").fetchone():
     sys.exit("table not created")
 
 
-def insert_data(data):
+def insert_data(data: Tuple[int, str]) -> None:
     con.execute("INSERT INTO user_data VALUES (?, ?)", data)
     con.commit()
 
 
-def get_file_id(msg_id):
+def get_file_id(msg_id: int) -> str:
     return con.execute("SELECT file_id FROM user_data WHERE message_id = ?", (msg_id,)).fetchone()[0]
 
 
-def delete_data_from_db(msg_id):
+def delete_data_from_db(msg_id: int) -> None:
     con.execute("DELETE FROM user_data WHERE message_id = ?", (msg_id,))
     con.commit()
