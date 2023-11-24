2dc190284cf7fd0f4e4805a134bc599eed55aab1
lain <hadjik777@gmail.com>
2023-11-24 22:09:28 +0300

    Change method names, refactor send_on_message()

--- database.py
+++ database.py
@@ -15,7 +15,7 @@ if "user_data" not in con.execute("SELECT name FROM sqlite_master").fetchone():
     sys.exit("table not created")
 
 
-def insert_data(data: Tuple[int, str]) -> None:
+def save_user_data(data: Tuple[int, str]) -> None:
     con.execute("INSERT INTO user_data VALUES (?, ?)", data)
     con.commit()
 
@@ -24,6 +24,6 @@ def get_file_id(msg_id: int) -> str:
     return con.execute("SELECT file_id FROM user_data WHERE message_id = ?", (msg_id,)).fetchone()[0]
 
 
-def delete_data_from_db(msg_id: int) -> None:
+def delete_user_data(msg_id: int) -> None:
     con.execute("DELETE FROM user_data WHERE message_id = ?", (msg_id,))
     con.commit()
--- handlers.py
+++ handlers.py
@@ -18,27 +18,26 @@ async def cmd_start(message: Message):
 @router.message(F.photo, ~IsAdminFilter())
 async def send_on_message(message: Message, bot: Bot):
     photo_id = message.photo[-1].file_id  # message.photo[-1] to get pic of biggest size
-    first_name = message.from_user.first_name
-    url = "https://telegram.me/" + message.from_user.username
-    caption = f"Send by [{first_name}]({url})"
+    full_name = message.from_user.first_name + " " + message.from_user.last_name
+    username = "@" + message.from_user.username
+    caption = f"Sent by {full_name} {username}"
 
     for admin_id in config.ADMIN_IDS:
         await bot.send_photo(
             chat_id=admin_id, photo=photo_id,
             reply_markup=get_post_or_not_kb(message.message_id, message.from_user.id),
-            caption=caption,
-            parse_mode="markdown"
+            caption=caption
         )
 
     params = (message.message_id, photo_id)
-    database.insert_data(params)
+    database.save_user_data(params)
 
 
 @router.callback_query(F.data.startswith("post_img"))
 async def post_img_to_channel(callback: CallbackQuery, bot: Bot):
     msg_id = callback.data.split("_")[-1]
     await bot.send_photo(config.CHANNEL_ID, database.get_file_id(int(msg_id)))
-    database.delete_data_from_db(int(msg_id))
+    database.delete_user_data(int(msg_id))
     await callback.message.delete()
 
 
@@ -48,4 +47,4 @@ async def dont_post_img_to_channel(callback: CallbackQuery, bot: Bot):
     sender_id = callback.data.split("_")[-2]
     await bot.send_message(sender_id, "Админу не понравилась твоя публикация((")
     await callback.message.delete()
-    database.delete_data_from_db(int(msg_id))
+    database.delete_user_data(int(msg_id))
